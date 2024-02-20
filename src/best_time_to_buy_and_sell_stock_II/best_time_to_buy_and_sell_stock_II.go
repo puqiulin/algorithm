83dae6257e6556b85528e8d182d329e38e447380
puqiulin <puqiulin20@gmail.com>
2024-02-20 14:25:58 +0800

    add BestTimeTo_BuyAndSellStockII

--- src/best_time_to_buy_and_sell_stock_II/best_time_to_buy_and_sell_stock_II.go
+++ src/best_time_to_buy_and_sell_stock_II/best_time_to_buy_and_sell_stock_II.go
@@ -1,1 +1,12 @@
 package best_time_to_buy_and_sell_stock_II
+
+func BestTimeTo_BuyAndSellStockII(prices []int) int {
+	n := len(prices)
+	maxProfit := 0
+	for i := range n - 1 {
+		if prices[i+1] > prices[i] {
+			maxProfit += prices[i+1] - prices[i]
+		}
+	}
+	return maxProfit
+}
